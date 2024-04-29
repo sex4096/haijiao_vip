7ff5ecdf7f8654ff5eb8b977d8f30f6e42524e30
John Doe <johndoe@example.com>
2024-04-30 02:06:51 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { initHookWebpack } from "./modules/webpack";
-import App from "./ui/app";
-import { Interceptor } from "./modules/interceptor";
-import { addAnalytics, addStyle } from "./modules/util";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { initHookWebpack } from "./modules/webpack";
+// import App from "./ui/app";
+// import { Interceptor } from "./modules/interceptor";
+// import { addAnalytics, addStyle } from "./modules/util";
 
-function initialed(vue: any, store: any, axios: any) {
-  const interceptor = new Interceptor(axios);
-  interceptor.initRequestInterceptor();
-  interceptor.initResponseInterceptor();
-}
+// function initialed(vue: any, store: any, axios: any) {
+//   const interceptor = new Interceptor(axios);
+//   interceptor.initRequestInterceptor();
+//   interceptor.initResponseInterceptor();
+// }
 
-function initSetting() {
-  const myButton = React.createElement(App, null);
-  const pluginDiv = document.createElement("div");
-  pluginDiv.id = "haijiao-vip-plugin";
-  document.body.appendChild(pluginDiv);
-  ReactDOM.render(myButton, pluginDiv);
-}
+// function initSetting() {
+//   const myButton = React.createElement(App, null);
+//   const pluginDiv = document.createElement("div");
+//   pluginDiv.id = "haijiao-vip-plugin";
+//   document.body.appendChild(pluginDiv);
+//   ReactDOM.render(myButton, pluginDiv);
+// }
 
-sessionStorage.setItem("pageOpen", "1");
-addAnalytics();
-addStyle();
-initSetting();
-initHookWebpack(initialed);
+// sessionStorage.setItem("pageOpen", "1");
+// addAnalytics();
+// addStyle();
+// initSetting();
+// initHookWebpack(initialed);
+
+console.log("hello world");
