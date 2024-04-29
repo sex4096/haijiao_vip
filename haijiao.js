99dcee7099833ee945b17c02da28a197d4462a5f
John Doe <johndoe@example.com>
2024-04-29 10:31:13 +0800

    小版本修订

--- haijiao.js
+++ haijiao.js
@@ -1,8 +1,8 @@
 
 // ==UserScript==
-// @name           haijiao-vip: 解锁海角社区VIP帖子,去广告
+// @name           haijiao-vip: 解锁海角社区 收费视频,VIP,去广告
 // @namespace      https://github.com/sex4096/haijiao_vip
-// @version        0.0.12
+// @version        0.0.13
 // @author         forgetme8
 // @description    解锁 海角社区(haijiao.com) 收费视频,VIP,并去除网站广告, TG频道:@svip_nav.本插件完全免费,请注意甄别,避免上当受骗.
 // @homepage       https://github.com/sex4096/haijiao_vip#readme
